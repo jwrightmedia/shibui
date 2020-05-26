182d6a5dc4f1ca96534b3880a52e2f8f6d8ff3bb
Josh Wright <josh-wright@Joshs-MacBook-Pro.local>
2020-05-26 10:46:29 -0600

    Add row fix for repeaters to ACF examples

--- acf_samples.php
+++ acf_samples.php
@@ -144,3 +144,29 @@
 		<?php echo do_shortcode( '[gravityform id="'.get_field('form_shortcode', 'option').'" title="false" description="false" ajax="true"]'); ?>
 	</div>
 </div>
+
+<!-- Row fix for repeaters - not proven yet -->
+
+<section class="home-page-selling-points container-fluid">
+        <?php $i = 1; ?>
+        <div class="row">
+            <?php
+            if (have_rows('selling_points')) :
+                while (have_rows('selling_points')) : the_row();
+            ?>
+            <div class="col-sm-3">
+                <img src="<?php echo get_sub_field('selling_point_icon')['url'] ?>" alt="">
+                <h3><?php the_sub_field('selling_point_title'); ?></h3>
+                <p><?php the_sub_field('selling_point_text'); ?></p>
+            </div>
+            <?php
+                    if ($i == 4) {
+                        echo '</div><div class="row">';
+                        $i = 0;
+                    }
+                    $i++;
+                endwhile;
+            endif;
+            ?>
+        </div>
+    </section>
