f4be0c97f1eef28bd02d4920d8c91a1abe55f72a
Josh Wright <josh@jwrightmedia.com>
2022-11-30 08:50:06 -0600

    Add ACF table field sample

--- acf_samples.php
+++ acf_samples.php
@@ -265,3 +265,35 @@
         </a>
     </a></article>
 <?php endwhile; endif; ?>
+
+<!-- ACF Table Field Generator https://wordpress.org/plugins/advanced-custom-fields-table-field/ -->
+<?php $table = get_sub_field( 'investment_requirements' );
+if ( ! empty ( $table ) ) {
+    echo '<table class="investment-table" border="0">';
+        if ( ! empty( $table['caption'] ) ) {
+            echo '<caption>' . $table['caption'] . '</caption>';
+        }
+        if ( ! empty( $table['header'] ) ) {
+            echo '<thead>';
+                echo '<tr>';
+                    foreach ( $table['header'] as $th ) {
+                        echo '<th>';
+                            echo $th['c'];
+                        echo '</th>';
+                    }
+                echo '</tr>';
+            echo '</thead>';
+        }
+        echo '<tbody>';
+            foreach ( $table['body'] as $tr ) {
+                echo '<tr>';
+                    foreach ( $tr as $td ) {
+                        echo '<td>';
+                            echo $td['c'];
+                        echo '</td>';
+                    }
+                echo '</tr>';
+            }
+        echo '</tbody>';
+    echo '</table>';
+}?>
